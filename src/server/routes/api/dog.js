5b188a3a5b20b8ea41b29ec777c2172d4bb6af09
Astrid Varga <astrid.varga@gmx.de>
2018-09-20 15:39:20 +0200

    Add get routes to display dog profiles of logged in user, all dog profiles and a single dog profile

--- src/server/routes/api/dog.js
+++ src/server/routes/api/dog.js
@@ -14,32 +14,33 @@ router.post("/new", checkLoggedIn, (req, res, next) => {
     if (req.body.getsAlongWith && !["dogs", "cats", "kids"].includes(dog.getsAlongWith))
         return res.status(400).send({ error: "Invalid value for getsAlongWith" });
 
+    // TODO - extract creation of new Dog in function
     let newDog = new Dog({
         user: req.user._id,
         name: dog.name,
         age: dog.age,
         breed: dog.breed,
-        getsAlongWith: {
-            kids: dog.getsAlongWith.kids,
-            dogs: dog.getsAlongWith.dogs,
-            cats: dog.getsAlongWith.cats
-        },
-        character: {
-            courage: dog.character.courage,
-            agility: dog.character.agility,
-            stubborn: dog.character.stubborn,
-            water: dog.character.water,
-            snuggly: dog.character.snuggly,
-            fightGamer: dog.character.fightGamer
-        },
+        // getsAlongWith: {
+        //     kids: dog.getsAlongWith.kids,
+        //     dogs: dog.getsAlongWith.dogs,
+        //     cats: dog.getsAlongWith.cats
+        // },
+        // character: {
+        //     courage: dog.character.courage,
+        //     agility: dog.character.agility,
+        //     stubborn: dog.character.stubborn,
+        //     water: dog.character.water,
+        //     snuggly: dog.character.snuggly,
+        //     fightGamer: dog.character.fightGamer
+        // },
         weight: dog.weight,
-        restrictions: {
-            maleDogs: dog.restrictions.maleDogs,
-            femaleDogs: dog.restrictions.femaleDogs,
-            traffic: dog.restrictions.traffic,
-            publicTransport: dog.restrictions.publicTransport,
-            car: dog.restrictions.car
-        },
+        // restrictions: {
+        //     maleDogs: dog.restrictions.maleDogs,
+        //     femaleDogs: dog.restrictions.femaleDogs,
+        //     traffic: dog.restrictions.traffic,
+        //     publicTransport: dog.restrictions.publicTransport,
+        //     car: dog.restrictions.car
+        // },
         aboutMe: dog.aboutMe,
         dogPictures: dog.dogPictures
     });
@@ -66,7 +67,33 @@ router.post("/new", checkLoggedIn, (req, res, next) => {
 router.get("/all", checkLoggedIn, (req, res, next) => {
     Dog.find({ user: req.user._id })
         .then(dog => {
-            res.send.json(dog);
+            res.send(dog);
+        })
+        .catch(error => {
+            next(error);
+        });
+});
+
+// Read all dog profiles
+router.get("/list", checkLoggedIn, (req, res, next) => {
+    Dog.find()
+        .then(doglist => {
+            res.send(doglist);
+        })
+        .catch(error => {
+            next(error);
+        });
+});
+
+// Read a single dog profile
+router.get("/profile/:id", checkLoggedIn, (req, res, next) => {
+    const dogId = req.params._id;
+    console.log("Dog Id: ", dogId);
+
+    Dog.findOne({ id: dogId })
+        .then(dog => {
+            console.log("Found a Dog!", dog);
+            res.send(dog);
         })
         .catch(error => {
             next(error);
