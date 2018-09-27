47945e6206c5fa8bd4d87cc4db04d2310266d124
Astrid Varga <astrid.varga@gmx.de>
2018-09-27 18:24:33 +0200

    finalize filter result dog card

--- src/app/js/Filter/List.jsx
+++ src/app/js/Filter/List.jsx
@@ -9,14 +9,18 @@ const List = ({ dogs }) => {
                 // return <li key={i}>{dog.name}</li>;
                 console.log("DOG: ", dog);
                 return (
-                    <div className="list-card" key={i}>
-                        <h2>{dog.name}</h2>
-                        <p>Breed: {dog.breed}</p>
-                        <p>Age: {dog.age}</p>
-                        <p>Id: {dog._id}</p>
-                        <Link className="link-button" to={`/dog/profile/${dog._id}`}>
-                            Show Profile
-                        </Link>
+                    <div className="dog-card" key={i}>
+                        <div className="dog-card-picture">
+                            <img src={dog.dogPicture} alt="" />
+                            <Link className="link-button dog-button" to={`/dog/profile/${dog._id}`}>
+                                Show Profile
+                            </Link>
+                        </div>
+                        <div className="dog-card-info">
+                            <h2>{dog.name}</h2>
+                            <p>Breed: {dog.breed}</p>
+                            <p>Age: {dog.age}</p>
+                        </div>
                     </div>
                 );
             })}
