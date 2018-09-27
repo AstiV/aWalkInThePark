238d2add6d39e3a5755b376e8e07b80c65098dee
Astrid Varga <astrid.varga@gmx.de>
2018-09-27 08:25:08 +0200

    Add Style to filter button on Home Screen

--- src/app/js/Home.jsx
+++ src/app/js/Home.jsx
@@ -8,9 +8,11 @@ const Home = props => {
         <div className="container">
             <div className="greeting-wrapper">
                 <h1>Hello, {props.user ? props.user.email : "Stranger"}!</h1>
-                <Link className="link-button button" to="/filter">
-                    Look for Walk Mates
-                </Link>
+                <div className="button-wrapper">
+                    <Link className="link-button button" to="/filter">
+                        Look for Walk Mates
+                    </Link>
+                </div>
             </div>
 
             <Walklist />
