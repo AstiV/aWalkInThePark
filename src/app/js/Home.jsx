8639530bb83cc46c84f6effda30b647dba53aa18
Astrid Varga <astrid.varga@gmx.de>
2018-12-09 16:17:16 +0100

    Fix Home Screen

--- src/app/js/Home.jsx
+++ src/app/js/Home.jsx
@@ -8,15 +8,31 @@ const Home = props => {
         <div className="container">
             <div className="greeting-wrapper">
                 <h1>Hello, {props.user ? props.user.email : "Stranger"}!</h1>
-                <div className="button-wrapper">
-                    <Link className="link-button button" to="/filter">
-                        Look for Walk Mates
+                {props.user && (
+                    <div className="button-wrapper">
+                        <Link className="link-button button" to="/filter">
+                            Look for Walk Mates
+                        </Link>
+                    </div>
+                )}
+            </div>
+            {!props.user && (
+                <p>
+                    <Link className="link" to="/auth/sign-up">
+                        Sign up &nbsp;
                     </Link>
+                    or
+                    <Link className="link" to="/auth/sign-in">
+                        &nbsp; Sign in &nbsp;
+                    </Link>
+                    to look for walk mates!
+                </p>
+            )}
+            {props.user && (
+                <div className="list-wrapper">
+                    <Walklist />
                 </div>
-            </div>
-            <div className="list-wrapper">
-                <Walklist />
-            </div>
+            )}
         </div>
     );
 };
